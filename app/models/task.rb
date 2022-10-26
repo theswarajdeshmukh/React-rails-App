230dee214ed9926858539fc38a201aa0c6149a56
Swaraj Deshmukh <swarajdeshmukh45@gmail.com>
2022-10-26 12:06:54 +0530

    Added constraint to make title not nullable

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+    MAX_TITLE_LENGTH = 125
+    validates :title, presence: true, length: { maximum: 125 }
 end
+  
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_24_095431) do
+ActiveRecord::Schema.define(version: 2022_10_26_063530) do
 
   create_table "tasks", force: :cascade do |t|
-    t.text "title"
+    t.text "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
