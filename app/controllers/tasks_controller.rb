a5a086b1b5d4771ef12f4dbeb030f761ffde24cc
Swaraj Deshmukh <swarajdeshmukh45@gmail.com>
2022-12-03 21:53:27 +0530

    Added ability to assign task to a user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,12 +8,12 @@ class TasksController < ApplicationController
   # for method names to end with a bang operator if they raise an exception.
 
   def index
-    tasks = Task.all
-    render status: :ok, json: { tasks: tasks }
+    tasks = Task.all.as_json(include: { assigned_user: { only: %i[name id] } })
+    respond_with_json({ tasks: tasks })
   end
 
   def show
-    respond_with_json({ task: @task })
+    respond_with_json({ task: @task, assigned_user: @task.assigned_user })
   end
 
   def update
@@ -39,6 +39,6 @@ class TasksController < ApplicationController
     end
 
     def task_params
-      params.require(:task).permit(:title)
+      params.require(:task).permit(:title, :assigned_user_id)
     end
 end
