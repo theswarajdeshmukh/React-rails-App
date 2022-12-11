c8cd4677e29b358b9ab3f5c0017adbb191a85827
Swaraj Deshmukh <swarajdeshmukh45@gmail.com>
2022-12-11 23:12:12 +0530

    Added ability to logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  skip_before_action :authenticate_user_using_x_auth_token
+  skip_before_action :authenticate_user_using_x_auth_token, only: :create
 
+  # Note that, Rails, by convention, expects a view file for the create action in the views directory.
+  # If that can't be found, then Rails will point out that the relevant template file is missing.
   def create
     @user = User.find_by!(email: login_params[:email].downcase)
     unless @user.authenticate(login_params[:password])
       respond_with_error(t("session.incorrect_credentials"), :unauthorized)
     end
   end
-  # Note that, Rails, by convention, expects a view file for the create action in the views directory. If that can't be found, then Rails will point out that the relevant template file is missing.
+
+  def destroy
+    @current_user = nil
+    # any other session cleanup tasks can be done here...
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   constraints(lambda { |req| req.format == :json }) do
     resources :tasks, except: %i[new edit], param: :slug
     resources :users, only: %i[index create]
-    resource :session, only: :create
+    resource :session, only: [:create, :destroy]
   end
   # Using lambda {|req| req.format==:json} as the constraint will make sure that
   # only JSON requests will match with the resources listed in the block.
